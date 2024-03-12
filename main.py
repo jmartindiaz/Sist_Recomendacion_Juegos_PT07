5572ab9052a22dd17761238cad37fc82303882c1
jmartindiaz <martin.diaz97@hotmail.com>
2024-03-11 23:16:08 -0300

    reviews no me da la cant registros esperada

---
+++ main.py
@@ -0,0 +1,5 @@
+# el main llamara a las librerias y llamara a los archivos en los que figuran las funciones aplicadas por ejemplo, en ETL
+
+# import LIBRERIAS
+# import archivos
+# import funciones
