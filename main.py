001e9d913052a47c0aa663f2d71e8cb57361ac78
jmartindiaz <martin.diaz97@hotmail.com>
2024-03-16 21:39:21 -0300

    transf grales: tratamiento de faltantes, duplicados y columnas sin utilidad

--- main.py
+++ main.py
@@ -12,33 +12,33 @@ import numpy as np
 app = FastApi()
 
 # Endpoints de nuestra API:
-## Inicio de la página
+    ## Inicio de la página
 @app.get("/")
 def index():
     return "Éste proyecto tiene por finalidad..."
 
-## PlayTimeGenre: deberá devolver el año con mas horas jugadas para el género ingresado.
+    ## PlayTimeGenre: deberá devolver el año con mas horas jugadas para el género ingresado.
 @app.get("/PlayTimeGenre/{genre}")
 def PlayTimeGenre(genre: str):
     return
 
-## UserForGenre: deberá devolver el usuario que acumula mas horas jugadas para el género ingresado 
-## y una lista de la acumulación de horas jugadas por año.
+    ## UserForGenre: deberá devolver el usuario que acumula mas horas jugadas para el género ingresado 
+    ## y una lista de la acumulación de horas jugadas por año.
 @app.get("/UserForGenre/{genre}")
 def UserForGenre(genre: str):
     return
 
-## UsersRecommend: deberá devolver el top 3 juegos mas recomendados por usuarios para el año ingresado.
+    ## UsersRecommend: deberá devolver el top 3 juegos mas recomendados por usuarios para el año ingresado.
 @app.get("/UsersRecommend/{anio}")
 def UsersRecommend(anio: int):
     return 
 
-## UsersNotRecommend: deberá devolver el top 3 juegos MENOS recomendados por usuarios para el año ingresado.
+    ## UsersNotRecommend: deberá devolver el top 3 juegos MENOS recomendados por usuarios para el año ingresado.
 @app.get("/UsersNotRecommend/{anio}")
 def UsersNotRecommend(anio: int):
     return
 
-## sentiment_analysis: deberá devolver una lista con la cantidad de registros de reseñas de usuarios que se encuentren
-## categorizados con un análisis de sentimiento.
+    ## sentiment_analysis: deberá devolver una lista con la cantidad de registros de reseñas de usuarios que se encuentren
+    ## categorizados con un análisis de sentimiento.
 @app.get("sentiment_analysis/{anio}")
 def sentiment_analysis()
